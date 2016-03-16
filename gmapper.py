630d6a05e2faf387d3d86e51ff6652a5cd0473f2
SkBlaz <SkBlaz@users.noreply.github.com>
2016-03-16 08:17:46 +0100

    Additional arguments

--- gmapper.py
+++ gmapper.py
@@ -9,28 +9,30 @@ by Blaz Skrlj
 import matplotlib as plt
 import argparse
 import numpy as np
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 class misc(object):
-    def getdata(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("fileinput", help="Input file, separated by , or ;", type=str)
-        parser.add_argument("-v", "--verbose", action="store_true",
-                            help="More program output")
-        args = parser.parse_args()        
-        filename = args.fileinput
+    def getdata(self, nameoffile):
         print ("Importing..")
-        output = np.genfromtxt(filename, delimiter=',')
+        output = np.genfromtxt(nameoffile, delimiter=',')
         return output
-    def display(self, data, candidates):
+    def display(self, data, candidates, fname, display):
         cans = np.array(candidates)
         plt.plot(cans[:,0],cans[:,1],'ro')
-        plt.axhline(int(np.mean(data)), color='r')
-        plt.axhline(int(np.mean(data)*1.5), color='r')
-        plt.axhline(int(np.mean(data)*0.5), color='r')
+        plt.axhline(max(cans[:,1])/4, color='r')
+        plt.axhline(max(cans[:,1]/2), color='r')
+        plt.axhline(int(max(cans[:,1]))*0.75, color='r')
+        red_patch = mpatches.Patch(color='red', label='75%, 50% and 25% \nof maximum frequency')
+        plt.legend(handles=[red_patch])
         plt.ylabel('Frequency of occurence')
         plt.xlabel('separate items')
         plt.title('Frequency distribution estimation graph')
-        plt.show(plt.hist(data,range(int(min(data)),int(max(data)),1)))
+        plt.hist(data,range(int(min(data)),int(max(data)),1))
+        ofile = fname[0:-3]+"png"
+        print ("Writing outfile: %s") % (ofile)
+        plt.savefig(ofile, bbox_inches='tight')
+        if display == True: 
+            plt.show()
         return;
     def keypoints(self, data):
         #vzame pare
@@ -80,29 +82,61 @@ class misc(object):
         finallist=[]
         for c in candidates:
             finallist.append(c[0])
-        print finallist
+        #print finallist
         part1 = finallist[:len(finallist)/2]
         part2 = finallist[len(finallist)/2:]
-        print ("Comparing deviations..")
-        print ("left|right: %d | %d"%(np.std(part1),np.std(part2)))
-        if np.std(part1)/4 > np.std(part2):
-            print ("Distributions are significantly different - you have two samples!")
-        else:
-            print ("You probably have only one sample!")
         
-                
-        print ("Frequency quartile distribution")
-        print(">25 percent: %d | >50 percent: %d | >75 percent: %d" % (count25,count50,count75))
+        meandiff=np.sqrt(np.power(np.mean(part2),2)-np.power(np.mean(part1),2))
+        rangeA = max(part1)-min(part1)
+        rangeB = max(part2)-min(part2)
+
+        if meandiff> rangeA or meandiff> rangeB:
+            print ("Comparing deviations..")
+            print ("left|right: %d | %d"%(np.std(part1),np.std(part2)))     
+            print ("Frequency quartile distribution")
+            print(">25 percent: %d | >50 percent: %d | >75 percent: %d" % (count25,count50,count75))
+            print ("Range difference A: %d\nRange difference B: %d\nDistribution distance: %d" % (rangeA,rangeB, meandiff))
+            span = (rangeA+rangeB)/2
+            dspan = meandiff/span
+            
+            print ("\nResults indicate two different distributions! Span: %d" % (dspan))
+        else:
+            print ("Comparing deviations..")
+            print ("left|right: %d | %d"%(np.std(part1),np.std(part2)))     
+            print ("Frequency quartile distribution")
+            print(">25 percent: %d | >50 percent: %d | >75 percent: %d" % (count25,count50,count75))
+            print (" Range difference A: %d\n Range difference B: %d\n Distribution distance: %d" % (rangeA,rangeB, meandiff))
+            print ("\nResults indicate only one distribution!")
+            span = (rangeA+rangeB)/2
+            dspan = meandiff/span
+            
+            print ("\nResults indicate two different distributions! Span: %d" % (dspan))
         return candidates
         
                 
             
         
 if __name__ == '__main__':
-    data = misc().getdata()
-    candidates = misc().keyalgo(misc().keypoints(data))
-    misc().display(data,candidates)
-    
+    parser = argparse.ArgumentParser(description="Genemapper tool!")
+    parser.add_argument("fileinput", help="Input file, separated by , or ;", type=str)
+    parser.add_argument('-d')
+    parser.add_argument('-view')
+    #parser.add_argument("-v", "--verbose", action="store_true",
+                        #help="More program output")
+    args = parser.parse_args()        
+    filename = args.fileinput
+    if args.d is not None:
+        data = misc().getdata(filename)
+        candidates = misc().keyalgo(misc().keypoints(data))
+        if args.view is not None:
+            show = True
+            misc().display(data,candidates, filename, show)
+        else:
+            show=False
+            misc().display(data,candidates, filename, show)        
+else:
+        data = misc().getdata(filename)
+        candidates = misc().keyalgo(misc().keypoints(data))
     
                              
     
